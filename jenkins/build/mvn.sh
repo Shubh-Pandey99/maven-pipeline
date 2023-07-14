22d4ec3142f9eeb146090ec9de32e851619bbf7c
EC2 Default User <ec2-user@ip-172-31-15-49.eu-west-1.compute.internal>
2023-07-14 19:48:11 +0000

    Fix path for build step

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,4 +4,5 @@ echo "******************************************"
 echo "************Building Jar******************"
 echo "******************************************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven "$@" 
+WORKSPACE=/home/ec2-user/maven_jenkins/jenkins_home/workspace/pipeline-maven-project
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven "$@" 
